a2eab75fd7880947f4743b29dded54a32a6ae91b
SANTHOSH KUMAR - SK <98202037+SANTHOSHKUMAR392002@users.noreply.github.com>
2025-03-23 18:46:23 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'event',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Change if your database is hosted elsewhere
+        'PORT': '3306',       # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
